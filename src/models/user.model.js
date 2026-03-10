cfc64cadf200734f5b0313807ccff7e386f0a53e
dharshini875 <137678264+dharshini875@users.noreply.github.com>
2026-03-10 17:04:37 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,35 @@
+const mongoose = require("mongoose")
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+/*
+TASK 1
+
+Implement password hashing using bcrypt.
+
+Requirements:
+- Use bcryptjs
+- Use mongoose pre("save") hook
+- Hash password only if modified
+- Use 10 salt rounds
+*/
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
