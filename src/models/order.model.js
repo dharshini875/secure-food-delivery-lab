c582940bddf67461804ba490ece1496be428c2fb
dharshini875 <137678264+dharshini875@users.noreply.github.com>
2026-03-10 17:06:17 +0530

    Update order.model.js

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,1 +1,16 @@
+/** Do not modify this file **/
+
+const mongoose = require("mongoose")
+
+const orderSchema = new mongoose.Schema({
+  orderId: String,
+  customerId: Number,
+  restaurant: String,
+  category: String,
+  amount: Number,
+  status: String,
+  orderDate: Date
+})
+
+module.exports = mongoose.model("Order", orderSchema)
 
